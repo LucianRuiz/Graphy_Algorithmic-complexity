e9a93d21953dd56e3609bb7362da107b94cc264c
LucianRuiz <97409520+LucianRuiz@users.noreply.github.com>
2023-11-04 19:07:41 -0500

    Add files via upload
    Creación de la lista de elementos con la misma marca del producto seleccionado

---
+++ marcas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Especifica la ID del producto seleccionado
+producto_seleccionado_id = 1  # Cambia 1 a la ID del producto que deseas seleccionar
+
+# Carga tu archivo CSV en un DataFrame de pandas
+productos_df = pd.read_csv('productos.csv')
+
+# Obtiene la marca del producto seleccionado
+marca_seleccionada = productos_df.loc[productos_df['id'] == producto_seleccionado_id, 'brand'].values[0]
+
+# Filtra los productos con la misma marca que el producto seleccionado
+productos_misma_marca = productos_df[productos_df['brand'] == marca_seleccionada]
+
+# Obtiene las IDs de los productos con la misma marca
+ids_productos_misma_marca = productos_misma_marca['id'].tolist()
+
+print(f"Productos con la misma marca ({marca_seleccionada}): {ids_productos_misma_marca}")
